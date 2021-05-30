622979c3f2e4369e12e230dd48dbb62f4fae3b08
StasEngel <69434834+StasEngel@users.noreply.github.com>
2021-05-30 12:49:36 +0300

    Deck and Player (partially)

--- src/Deck.java
+++ src/Deck.java
@@ -1,10 +1,39 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class Deck
 {
-    void addCard(Card card){}
+    private Stack<Card> deck;
 
-    Card removeTopCard(){}
+    public Deck(boolean condition)
+    {
+        this.deck = null;
+        if (condition)
+        {
+            for (Shape shape: Shape.values())
+            {
+                for (int i = 1; i <= 13; i++)
+                {
+                    deck.push(new Card(i, shape));
+                }
+            }
+        }
+    }
 
-    boolean isEmpty(){}
+    void addCard(Card card)
+    {
+        this.deck.push(card);
+    }
+
+    Card removeTopCard()
+    {
+        return this.deck.pop();
+    }
+
+    boolean isEmpty()
+    {
+        return this.deck.isEmpty();
+    }
 
     void shuffle(){}
 
--- src/Player.java
+++ src/Player.java
@@ -1,9 +1,25 @@
+import java.util.ArrayList;
+
 public class Player
 {
+    private Deck game_deck;
+    private Deck winning_deck;
+    private String name;
+
+    public Player(String name)
+    {
+        this.name = name;
+        game_deck = new Deck(false);
+        winning_deck = new Deck(false);
+    }
+
     void drawCard(){}
 
     boolean outOfCards(){}
 
     @Override
-    public String toString(){}
+    public String toString()
+    {
+        return this.name;
+    }
 }
