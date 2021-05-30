25cfa92f3dd31ca1d4c7b544e191c5eb44c4cec8
StasEngel <69434834+StasEngel@users.noreply.github.com>
2021-05-30 13:21:25 +0300

    Deck (full) and Wargame (initialize variables)

--- src/Deck.java
+++ src/Deck.java
@@ -35,7 +35,24 @@ public class Deck
         return this.deck.isEmpty();
     }
 
-    void shuffle(){}
+    void shuffle()
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            int index1 = Main.rnd.nextInt(52);
+            int index2 = -1;
+            while (index1 != index2) //get random index until we found one different from index1
+            {
+                index2 = Main.rnd.nextInt(52);
+            }
 
-
+            //swap cards
+            Card temp;
+            Card card1 = this.deck.elementAt(index1);
+            Card card2 = this.deck.elementAt(index2);
+            temp = card1;
+            card1 = card2;
+            card2 = temp;
+        }
+    }
 }
--- src/WarGame.java
+++ src/WarGame.java
@@ -1,6 +1,15 @@
-public class WarGame
-{
-    void initializeGame(){}
+public class WarGame {
+    private Player player1;
+    private Player player2;
+
+    public WarGame(String player1_name, String player2_name) {
+        this.player1 = new Player(player1_name);
+        this.player2 = new Player(player2_name);
+    }
+
+    void initializeGame() {
+
+    }
 
     String start(){}
 }
